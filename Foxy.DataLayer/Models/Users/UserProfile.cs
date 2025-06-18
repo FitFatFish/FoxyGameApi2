563731f5f81b864039f9a539a537ebd1500ec1e3
Ehsan <ershadi.ehsan@gmail.com>
2025-06-18 16:14:05 +0100

    feat: added user profile model

---
+++ Foxy.DataLayer/Models/Users/UserProfile.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Foxy.DataLayer.Models.FoxyGame;
+using Foxy.DataLayer.Models.Support;
+
+namespace Foxy.DataLayer.Models.Users
+{
+    public class UserProfile
+    {
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } 
+        
+        public string AccountId { get; set; }
+        public string Bio { get; set; }
+        public string ProfileImage { get; set; }
+        public string HeaderImage { get; set; }
+
+        public ICollection<MatchMember>  MatchMembers { get; set; }
+        public ICollection<UserGameScore> GameScores { get; set; }
+        public ICollection<UserItem> UserItems { get; set; }
+        public ICollection<SuggestionVote> SuggestionVotes { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
