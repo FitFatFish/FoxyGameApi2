7acb1e028a67b68fe16b87171402f985ded891bb
Ehsan <ershadi.ehsan@gmail.com>
2025-06-19 12:40:59 +0100

    feat: create migration

---
+++ Foxy.DataLayer/Migrations/20250619112213_InitialCreate.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Foxy.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "FoxyUserInfos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    AccountId = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Bio = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ProfileImageGuid = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    HeaderImageGuid = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoxyUserInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeaderImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ImageGuid = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    RequiredLevel = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeaderImages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suggestions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    ConfirmedDescription = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    LikeCount = table.Column<int>(type: "integer", nullable: false),
+                    DislikeCount = table.Column<int>(type: "integer", nullable: false),
+                    Published = table.Column<int>(type: "integer", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suggestions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_FoxyUserInfos_CreatedBy",
+                        column: x => x.CreatedBy,
+                        principalTable: "FoxyUserInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    GameCategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ImageGuid = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Documentation = table.Column<string>(type: "character varying(4096)", maxLength: 4096, nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_GameCategories_GameCategoryId",
+                        column: x => x.GameCategoryId,
+                        principalTable: "GameCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SuggestionVotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    SuggestionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserProfileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Like = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuggestionVotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SuggestionVotes_FoxyUserInfos_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "FoxyUserInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SuggestionVotes_Suggestions_SuggestionId",
+                        column: x => x.SuggestionId,
+                        principalTable: "Suggestions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchGames",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    GameId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BeginDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchGames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MatchGames_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoreItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    GameId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Level = table.Column<int>(type: "integer", nullable: false),
+                    ImageGuid = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoreItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StoreItems_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchMembers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    MatchId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserProfileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TeamName = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    ResultType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MatchMembers_FoxyUserInfos_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "FoxyUserInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MatchMembers_MatchGames_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "MatchGames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    UserProfileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StoreItemId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserItems_FoxyUserInfos_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "FoxyUserInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserItems_StoreItems_StoreItemId",
+                        column: x => x.StoreItemId,
+                        principalTable: "StoreItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GameCategoryId",
+                table: "Games",
+                column: "GameCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchGames_GameId",
+                table: "MatchGames",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchMembers_MatchId",
+                table: "MatchMembers",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchMembers_UserProfileId",
+                table: "MatchMembers",
+                column: "UserProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreItems_GameId",
+                table: "StoreItems",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuggestionVotes_SuggestionId",
+                table: "SuggestionVotes",
+                column: "SuggestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuggestionVotes_UserProfileId",
+                table: "SuggestionVotes",
+                column: "UserProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_CreatedBy",
+                table: "Tickets",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserItems_StoreItemId",
+                table: "UserItems",
+                column: "StoreItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserItems_UserProfileId",
+                table: "UserItems",
+                column: "UserProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HeaderImages");
+
+            migrationBuilder.DropTable(
+                name: "MatchMembers");
+
+            migrationBuilder.DropTable(
+                name: "SuggestionVotes");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "UserItems");
+
+            migrationBuilder.DropTable(
+                name: "MatchGames");
+
+            migrationBuilder.DropTable(
+                name: "Suggestions");
+
+            migrationBuilder.DropTable(
+                name: "FoxyUserInfos");
+
+            migrationBuilder.DropTable(
+                name: "StoreItems");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "GameCategories");
+        }
+    }
+}
