9067a8eee9d313767481b6f48201ffd91082e9dc
Ehsan <ershadi.ehsan@gmail.com>
2025-06-19 13:14:53 +0100

    feat: create unit of work

---
+++ Foxy.WebApi/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Foxy.WebApi.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<T> Repository<T>() where T : class;
+    Task<int> SaveChangesAsync();
+}
---
+++ Foxy.WebApi/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using Foxy.DataLayer.DBContext;
+
+namespace Foxy.WebApi.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly FoxyDbContext _context;
+    private readonly ConcurrentDictionary<Type, object> _repositories = new();
+
+    public UnitOfWork(FoxyDbContext context)
+    {
+        _context = context;
+    }
+
+    public IRepository<T> Repository<T>() where T : class
+    {
+        return (IRepository<T>)_repositories.GetOrAdd(typeof(T), _ => new Repository<T>(_context));
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
