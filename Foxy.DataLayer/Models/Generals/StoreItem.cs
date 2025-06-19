f954f732fc1703448fa4ab41701960e51ea49ee0
Ehsan <ershadi.ehsan@gmail.com>
2025-06-19 09:41:06 +0100

    fix: stire items model

--- Foxy.DataLayer/Models/Generals/StoreItem.cs
+++ Foxy.DataLayer/Models/Generals/StoreItem.cs
@@ -1,37 +1,21 @@
 ﻿using Foxy.DataLayer.Models.Games;
 using Foxy.DataLayer.Models.Users;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using Foxy.Core.Infrastructures.Enums;
 
 namespace Foxy.DataLayer.Models.Generals
 {
     public class StoreItem
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Id { get; set; } 
-
+        public Guid Id { get; set; }
 
         public Guid? GameId { get; set; }
-
         public string Title { get; set; }
-
         public int Level { get; set; }
-
-        public string Image { get; set; } //size(40)
-
+        public string Image { get; set; }
         public StoreTypeEnum Type { get; set; }
 
-        // CUD
-        public Guid? CreatedBy { get; set; } // CreatedBy , CreatedAt
-        public Guid? UpdatedBy { get; set; } // UpdatedBy
+        public Game Game { get; set; }
 
-        [Required]
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdateAt { get; set; }
-
-        public Game game { get; set; }
-
-        public List<UserItem> userItems { get; set; }
+        public ICollection<UserItem> UserItems { get; set; }
     }
 }
