561ed4b4756c11bd05827be05bfa2eece0423f00
Ehsan <ershadi.ehsan@gmail.com>
2025-06-19 13:15:34 +0100

    create game category controller

---
+++ Foxy.WebApi/Controllers/GameCategoryController.cs
@@ -0,0 +1,50 @@
+using Foxy.DataLayer.Models.Games;
+using Foxy.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foxy.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameCategoryController : ControllerBase
+    {
+        private readonly GameCategoryService _service;
+
+        public GameCategoryController(GameCategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var item = await _service.GetByIdAsync(id);
+            return item == null ? NotFound() : Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GameCategory entity)
+        {
+            var created = await _service.CreateAsync(entity);
+            return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, GameCategory entity)
+        {
+            if (id != entity.Id) return BadRequest();
+            await _service.UpdateAsync(entity);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deleted = await _service.DeleteAsync(id);
+            return deleted ? NoContent() : NotFound();
+        }
+    }
+}
