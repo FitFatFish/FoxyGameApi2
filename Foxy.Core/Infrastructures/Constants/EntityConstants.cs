d23658b3bfe06ca72851e4516a1f2b829230e8c9
Ehsan <ershadi.ehsan@gmail.com>
2025-06-19 11:21:11 +0100

    create entity constants

---
+++ Foxy.Core/Infrastructures/Constants/EntityConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Foxy.Core.Infrastructures.Constants;
+
+public static class EntityConstants
+{
+    public const int ImageGuidSize = 40;
+    public const int DocumentTextSize = 4 * 1024;
+    public const int GeneralTitleSize = 50;
+}
