2a24a009b2e2fa1d81c7e99cc9e4a55d7c7e6e13
haghaniat <amin.haghaniat@gmail.com>
2025-07-14 12:32:22 +0330

    MatchMember controller

---
+++ Foxy.Core/Dtos/ResultDtos/MatchMemberResDto.cs
@@ -0,0 +1,21 @@
+﻿using Foxy.Core.Infrastructures.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foxy.Core.Dtos.ResultDtos
+{
+    public class MatchMemberResDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid MatchId { get; set; }
+        public Guid UserProfileId { get; set; }
+
+        public string TeamName { get; set; }
+        public int Score { get; set; }
+        public ResultTypeEnum ResultType { get; set; }
+    }
+}
---
+++ Foxy.WebApi/Controllers/MatchMemberController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Foxy.Core.Dtos.RequestDtos;
+using Foxy.Core.Dtos.ResultDtos;
+using Foxy.DataLayer.Models.Games;
+using Foxy.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foxy.WebApi.Controllers
+{
+    public class MatchMemberController : Controller
+    {
+        private readonly MatchMemberService _service;
+        private readonly IMapper _mapper;
+
+        public MatchMemberController(MatchMemberService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var item = await _service.GetByIdAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            var result = _mapper.Map<MatchMemberReqDto>(item);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MatchMemberReqDto reqentity)
+        {
+            var userid = User.Claims.FirstOrDefault(c => c.Type == "userid")?.Value;
+            var entity = _mapper.Map<MatchMember>(reqentity);
+           
+            var created = await _service.CreateAsync(entity);
+            return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, MatchMemberReqDto reqentity)
+        {
+            //var entity = await _service.GetByIdAsync(id);
+            //entity.Title = reqentity.Title;
+            var entity = _mapper.Map<MatchMember>(reqentity);
+            if (id != entity.Id) return BadRequest();
+            await _service.UpdateAsync(entity);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deleted = await _service.DeleteAsync(id);
+            return deleted ? NoContent() : NotFound();
+        }
+    }
+}
