78b5d660b3057c7e0660dcdb410ca0f50e9187ff
Ehsan <ershadi.ehsan@gmail.com>
2025-07-24 13:28:11 +0100

    clean up

--- Foxy.Core/Dtos/RequestDtos/MatchMemberReqDto.cs
+++ Foxy.Core/Dtos/RequestDtos/MatchMemberReqDto.cs
@@ -1,21 +1,15 @@
 ﻿using Foxy.Core.Infrastructures.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Foxy.Core.Dtos.RequestDtos
+namespace Foxy.Core.Dtos.RequestDtos;
+
+public class MatchMemberReqDto
 {
-    public class MatchMemberReqDto
-    {
-        public Guid? Id { get; set; }
+    public Guid? Id { get; set; }
 
-        public Guid MatchId { get; set; }
-        public Guid UserProfileId { get; set; }
+    public Guid MatchId { get; set; }
+    public Guid UserProfileId { get; set; }
 
-        public string TeamName { get; set; }
-        public int Score { get; set; }
-        public ResultTypeEnum ResultType { get; set; }
-    }
+    public string TeamName { get; set; }
+    public int Score { get; set; }
+    public ResultTypeEnum ResultType { get; set; }
 }
