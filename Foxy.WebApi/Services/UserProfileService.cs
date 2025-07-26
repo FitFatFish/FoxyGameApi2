96661f8b6c48fe322786375a50c469184dcc7f6c
haghaniat <amin.haghaniat@gmail.com>
2025-07-27 02:01:48 +0330

    Add UserProfileService

---
+++ Foxy.WebApi/Services/UserProfileService.cs
@@ -0,0 +1,35 @@
+﻿using Foxy.DataLayer.Models.Games;
+using Foxy.DataLayer.Models.Users;
+using Foxy.WebApi.Repositories;
+
+namespace Foxy.WebApi.Services
+{
+    public class UserProfileService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<UserProfile> _repository;
+
+        public UserProfileService(IUnitOfWork unitOfWork, IRepository<UserProfile> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<UserProfile>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<UserProfile?> GetByIdAsync(Guid id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<UserProfile> CreateAsync(UserProfile entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
